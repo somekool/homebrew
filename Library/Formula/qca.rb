401938c19c773f8ecce72b511390e6e36b67db94
somekool <somekool@gmail.com>
2013-09-05 12:27:23 -0400

    revert changes from upstream on qca

--- Library/Formula/qca.rb
+++ Library/Formula/qca.rb
@@ -1,9 +1,8 @@
 require 'formula'
 
 class Qca < Formula
-  url 'http://delta.affinix.com/download/qca/2.0/qca-2.0.3.tar.bz2'
   homepage 'http://delta.affinix.com/qca/'
-  md5 'fc15bd4da22b8096c51fcfe52d2fa309'
+  url 'http://delta.affinix.com/download/qca/2.0/qca-2.0.3.tar.bz2'
   sha1 '9c868b05b81dce172c41b813de4de68554154c60'
 
   depends_on 'qt'
